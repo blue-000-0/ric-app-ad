b1d732de78ffbca66e1fba5633b8b488155ccb07
Winnie <112544390+blue-000-0@users.noreply.github.com>
2023-05-26 14:08:06 +0800

    Update database.py

--- src/database.py
+++ src/database.py
@@ -116,7 +116,7 @@ class DATABASE(object):
 
     def query(self, query):
         try:
-            result = self.client.query(query)
+            result = self.client.query_api(query)
         except (RequestException, InfluxDBClientError, InfluxDBServerError, ConnectionError) as e:
             logger.error('Failed to connect to influxdb: {}'.format(e))
             result = False
