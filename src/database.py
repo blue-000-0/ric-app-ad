42871cab9c325807ae501555763d72ac7212cb1c
Winnie <112544390+blue-000-0@users.noreply.github.com>
2023-06-12 17:31:24 +0800

    Update database.py

--- src/database.py
+++ src/database.py
@@ -117,7 +117,7 @@ class DATABASE(object):
        
         self.data = pd.concat(data_frames)
         self.data = pd.DataFrame(self.data)
-        logger.info(self.data.columns.tolist())
+        logger.info(self.data.head(1))
             
     def write_anomaly(self, df, meas='AD'):
         """Write data method for a given measurement
