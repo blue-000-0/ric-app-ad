54aeec27073b6bca860809c34dfe6b73dc7479f0
Winnie <112544390+blue-000-0@users.noreply.github.com>
2023-06-13 16:42:54 +0800

    Update database.py

--- src/database.py
+++ src/database.py
@@ -98,6 +98,7 @@ class DATABASE(object):
             query += ' |> filter(fn: (r) => r["_measurement"] == "UeMetrics") '
             query += ' |> filter(fn: (r) => r["_field"] == "DRB_UEThpDl" or r["_field"] == "Viavi_UE_Rsrp" or r["_field"] == "Viavi_UE_Rsrq" or r["_field"] == "Viavi_UE_RsSinr" or r["_field"] == "RRU_PrbUsedDl" or r["_field"] == "Viavi_UE_anomalies") '
             query += ' |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
+            query += ' |> limit(n: 1)'
         elif train:
             query += ' |> range(start: -2d, stop: -1d) '
             query += ' |> filter(fn: (r) => r["_measurement"] == "UeMetrics") '
