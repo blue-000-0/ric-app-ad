5781c0b0d4902dd5469ebc04dd59a095819ee4ea
Winnie <112544390+blue-000-0@users.noreply.github.com>
2023-06-13 12:18:12 +0800

    Update database.py

--- src/database.py
+++ src/database.py
@@ -93,17 +93,17 @@ class DATABASE(object):
         self.data = None
         query = 'from(bucket:"{}")'.format(self.bucket)
         if not train and not valid and not limit:
-            query += ' |> range(start: -s) '
+            query += ' |> range(start: -30m) '
             query += ' |> filter(fn: (r) => r["_measurement"] == "UeMetrics") '
             query += ' |> filter(fn: (r) => r["_field"] == "DRB_UEThpDl" or r["_field"] == "Viavi_UE_Rsrp" or r["_field"] == "Viavi_UE_Rsrq" or r["_field"] == "Viavi_UE_RsSinr" or r["_field"] == "RRU_PrbUsedDl" or r["_field"] == "Viavi_UE_anomalies") '
             query += ' |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
         elif train:
-            query += ' |> range(start: -10m, stop: -5m) '
+            query += ' |> range(start: -60m, stop: -55m) '
             query += ' |> filter(fn: (r) => r["_measurement"] == "UeMetrics") '
             query += ' |> filter(fn: (r) => r["_field"] == "DRB_UEThpDl" or r["_field"] == "Viavi_UE_Rsrp" or r["_field"] == "Viavi_UE_Rsrq" or r["_field"] == "Viavi_UE_RsSinr" or r["_field"] == "RRU_PrbUsedDl" or r["_field"] == "Viavi_UE_anomalies") '
             query += ' |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
         elif valid:
-            query += ' |> range(start: -5m) '
+            query += ' |> range(start: -45m, stop: -40m) '
             query += ' |> filter(fn: (r) => r["_measurement"] == "UeMetrics") '
             query += ' |> filter(fn: (r) => r["_field"] == "DRB_UEThpDl" or r["_field"] == "Viavi_UE_Rsrp" or r["_field"] == "Viavi_UE_Rsrq" or r["_field"] == "Viavi_UE_RsSinr" or r["_field"] == "RRU_PrbUsedDl" or r["_field"] == "Viavi_UE_anomalies") '
             query += ' |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
