2cedf39742a1b0ae4ef85ddc4e9eabfb81e6259b
Winnie <112544390+blue-000-0@users.noreply.github.com>
2023-06-15 16:18:25 +0800

    Update database.py

--- src/database.py
+++ src/database.py
@@ -100,17 +100,15 @@ class DATABASE(object):
             query += ' |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
             query += ' |> limit(n: 1)'
         elif train:
+            query += ' |> range(start: -40m, stop: -20m) '
+            query += ' |> filter(fn: (r) => r["_measurement"] == "UeMetrics") '
+            query += ' |> filter(fn: (r) => r["_field"] == "DRB_UEThpDl" or r["_field"] == "Viavi_UE_Rsrp" or r["_field"] == "Viavi_UE_Rsrq" or r["_field"] == "Viavi_UE_RsSinr" or r["_field"] == "RRU_PrbUsedDl" or r["_field"] == "Viavi_UE_anomalies") '
+            query += ' |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
+        elif valid:
             query += ' |> range(start: -20m, stop: -10m) '
             query += ' |> filter(fn: (r) => r["_measurement"] == "UeMetrics") '
             query += ' |> filter(fn: (r) => r["_field"] == "DRB_UEThpDl" or r["_field"] == "Viavi_UE_Rsrp" or r["_field"] == "Viavi_UE_Rsrq" or r["_field"] == "Viavi_UE_RsSinr" or r["_field"] == "RRU_PrbUsedDl" or r["_field"] == "Viavi_UE_anomalies") '
             query += ' |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
-            query += ' |> limit(n: 15)'
-        elif valid:
-            query += ' |> range(start: -10m, stop: -5m) '
-            query += ' |> filter(fn: (r) => r["_measurement"] == "UeMetrics") '
-            query += ' |> filter(fn: (r) => r["_field"] == "DRB_UEThpDl" or r["_field"] == "Viavi_UE_Rsrp" or r["_field"] == "Viavi_UE_Rsrq" or r["_field"] == "Viavi_UE_RsSinr" or r["_field"] == "RRU_PrbUsedDl" or r["_field"] == "Viavi_UE_anomalies") '
-            query += ' |> pivot(rowKey: ["_time"], columnKey: ["_field"], valueColumn: "_value") '
-            query += ' |> limit(n: 10)'
         elif limit:
             query += ' |> range(start: -1m limit)'+str(limit)
     
